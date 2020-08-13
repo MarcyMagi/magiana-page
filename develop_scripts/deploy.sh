b98bd4e8d97c6d461b08f3da853c1aee9b785f1b
Marcy <mm.leitea@outlook.com>
2020-08-13 14:02:24 -0300

    deploy

--- develop_scripts/deploy.sh
+++ develop_scripts/deploy.sh
@@ -6,16 +6,19 @@ set -e
 # build
 npm run build
 
+# navigate into the build output directory
+cd dist
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git --work-tree="dist" add *
+git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push origin gh-pages
+git push origin gh-pages --force
 
-rm -r dist
+cd -
