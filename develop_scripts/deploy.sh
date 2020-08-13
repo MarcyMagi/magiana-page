57494b33b9de50edd3c6ad2c54b3b1997e7e0859
Marcy <mm.leitea@outlook.com>
2020-08-13 20:22:48 -0300

    i'm kinda lost

--- develop_scripts/deploy.sh
+++ develop_scripts/deploy.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env sh
 
 # abort on errors
-# set -e
+set -e
 
-# cd dist
+cd dist
 
 
-# git config --global user.email $1
-# git config --global user.name $2
+git init
+git add -A
+git commit -m 'deploy'
 
-# git init
-# git add -A
-# git commit -m 'deploy'
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git remote add origin https://github.com/MarcyMagi/magiana-page.git
+git push origin master:gh-pages --force
 
-# # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git remote add origin https://github.com/MarcyMagi/magiana-page.git
-# git push origin master:gh-pages --force
+cd -
 
-# cd -
+rm -rf dist
 
-# rm -rf dist
-
-echo $1 $2
