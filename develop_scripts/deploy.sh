8e5a493d0b853af6bb868fbe95fd6a4ff21cc5fe
Marcy <mm.leitea@outlook.com>
2020-08-13 13:57:00 -0300

    reseting?

---
+++ develop_scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git --work-tree="dist" add *
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push origin gh-pages
+
+rm -r dist
