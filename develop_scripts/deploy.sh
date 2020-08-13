373b081c7e14f018ca8f07e1ab26f28a1810d58c
Marcy <mm.leitea@outlook.com>
2020-08-13 20:03:45 -0300

    using env var in workflow

--- develop_scripts/deploy.sh
+++ develop_scripts/deploy.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env sh
 
 # abort on errors
-set -e
+# set -e
 
-cd dist
+# cd dist
 
-git init
-git add -A
-git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git remote add origin https://github.com/MarcyMagi/magiana-page.git
-git push origin master:gh-pages --force
+# git config --global user.email $1
+# git config --global user.name $2
 
-cd -
+# git init
+# git add -A
+# git commit -m 'deploy'
 
-rm -rf dist
+# # if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git remote add origin https://github.com/MarcyMagi/magiana-page.git
+# git push origin master:gh-pages --force
+
+# cd -
+
+# rm -rf dist
+
+echo $1 $2
